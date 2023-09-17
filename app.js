633d47678ac6c2f381bc2fceabb3daf580baf43c
skt0725 <skt0725@gmail.com>
2023-09-18 03:40:20 +0900

    clock

---
+++ app.js
@@ -0,0 +1,16 @@
+const clockTitle = document.querySelector(".js-clock");
+
+function getClock() {
+    const date = new Date()
+    const dateString = date.getFullYear() + '년 ' + 
+        (date.getMonth() + 1) + '월 ' + 
+        date.getDate() + '일 ' + 
+        date.getHours() + '시 ' + 
+        date.getMinutes() + '분';
+    clockTitle.innerText = dateString;
+
+}
+
+getClock();
+setInterval(getClock, 1000);
+
