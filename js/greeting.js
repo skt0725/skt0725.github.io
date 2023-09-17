fcbd3dca12b4e06156ef60c622abb4393cef2f17
skt0725 <skt0725@gmail.com>
2023-09-18 05:46:32 +0900

    greeting added

---
+++ js/greeting.js
@@ -0,0 +1,27 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.ariaValueMax;
+    localStorage.setItem("username", username);
+    paintGreetings(username);
+}
+loginForm.add("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem("username");
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername)
+}
